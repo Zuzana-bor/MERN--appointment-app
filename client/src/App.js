7358617bce6f2453f61ef6b9ff5818574a5a1671
Zuzana-bor <z.kasikova@gmail.com>
2025-01-18 10:13:33 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <>
-      <div className="bg-dark">
-        <h1 className="text-danger"> hello world</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
